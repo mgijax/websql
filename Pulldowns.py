a7220d8850ef83c0ee25b57f07b94ca7ada18301
Jonathan Beal <Jon.Beal@jax.org>
2010-09-02 12:53:53 +0000

    added handling for different RDBMS (postgres, mysql)

--- Pulldowns.py
+++ Pulldowns.py
@@ -1,6 +1,7 @@
 # defines functions to create javascript-enabled pulldowns:
 
 import string
+import sys
 
 class Pulldowns:
 	def __init__ (self, servermap):
@@ -9,15 +10,41 @@ class Pulldowns:
 		self.data = servermap.nested_dict()
 		return
 
-	def database (self, selected_server = None, selected_database = None):
-		servers = self.data.keys()
-		servers.sort()
+	def dbms (self, selected_dbms = None):
+		dbms_list = self.data.keys()
+		dbms_list.sort()
+		if selected_dbms is None:
+			selected_dbms = dbms_list[0]
+
+		list = [ '<SELECT NAME=DBMS1 onChange="do_dbms(DBMS1.options[DBMS1.selectedIndex].text)">' ]
+		for dbms in dbms_list:
+			if dbms == selected_dbms:
+				list.append ('<OPTION SELECTED> %s' % dbms)
+			else:
+				list.append ('<OPTION> %s' % dbms)
+		list.append ('</SELECT>')
+		return '\n'.join (list) 
+
+	def database (self,
+		selected_dbms = None,
+		selected_server = None, 
+		selected_database = None
+		):
+		if selected_dbms is None:
+			dbms_list = self.data.keys()
+			dbms_list.sort()
+			selected_dbms = dbms_list[0]
+
 		if selected_server is None:
+			servers = self.data[selected_dbms].keys()
+			servers.sort()
 			selected_server = servers[0]
-		databases = self.data[selected_server].keys()
+		
+		databases = self.data[selected_dbms][selected_server].keys()
 		databases.sort ()
 		if selected_database is None:
 			selected_database = databases[0]
+
 		list = [ '<SELECT NAME=database>' ]
 		for db in databases:
 			if db == selected_database:
@@ -27,13 +54,22 @@ class Pulldowns:
 		list.append ('</SELECT>')
 		return string.join (list, '\n')
 
-	def server (self, selected_server = None):
-		servers = self.data.keys()
+	def server (self,
+		selected_dbms = None,
+		selected_server = None
+		):
+		if selected_dbms is None:
+			dbms_list = self.data.keys()
+			dbms_list.sort()
+			selected_dbms = dbms_list[0]
+
+		servers = self.data[selected_dbms].keys()
 		servers.sort()
 		if selected_server is None:
 			selected_server = servers[0]
-		list = [ '<SELECT NAME=server onChange="do_server(server.options[server.selectedIndex].text)">' ]
-		servers = self.data.keys()
+
+		list = [ '<SELECT NAME=server onChange="do_server(DBMS1.options[DBMS1.selectedIndex].text, server.options[server.selectedIndex].text)">' ]
+		servers = self.data[selected_dbms].keys()
 		servers.sort()
 		for srv in servers:
 			if srv == selected_server:
@@ -45,8 +81,15 @@ class Pulldowns:
 
 	def code (self):
 		js = [	'<SCRIPT>',
+			'function set (dbs, value) {',
+			' for (var i=0; i < dbs.options.length; i++) {',
+			'  if (dbs.options[i].text == value) {',
+			'   dbs.options[i].selected=true;',
+			'   }',
+			'  }',
+			' }',
 			'function populate (dbs, values) {',
-			' // dbs is a select object for the databases',
+			' // dbs is a select object for the databases or servers',
 			' // values is an Array of strings',
 			' var selectedItem = dbs.options[dbs.options.selectedIndex].text',
 			' var selectedAny = false',
@@ -65,22 +108,47 @@ class Pulldowns:
 			' }',
 			'}',
 			'',
-			'function do_server (server) {',
+			'function do_server (dbms, server) {',
 			' // server is a string identifying which server',
 			' var dbs = document.forms[0].database',
 			]
 
-		for server in self.data.keys():
-			databases = self.data[server].keys()
+		for dbms in self.data.keys():
+		    for server in self.data[dbms].keys():
+			databases = self.data[dbms][server].keys()
 			databases.sort()
-			js.append (' if (server=="%s") {' % server)
+			js.append (' if ((dbms == "%s") && (server=="%s")) {' % (dbms, server))
 			js.append ('  populate (dbs, new Array ("%s"))' % \
 				string.join (databases, '","'))
 			js.append (' }')
 		js.append ('}')
+
+		js = js + [
+			'function do_dbms (dbms) {',
+			' // dbms is a string identifying which RDBMS',
+			' var servers = document.forms[0].server',
+			]
+		for dbms in self.data.keys():
+			servers = self.data[dbms].keys()
+			servers.sort()
+			js.append (' if (dbms=="%s") { ' % dbms)
+			js.append ('  populate (servers, new Array ("%s"))' %\
+				'","'.join (servers))
+			js.append (' }')
+		js.append (' do_server(document.forms[0].DBMS1.options[document.forms[0].DBMS1.selectedIndex].text, document.forms[0].server.options[document.forms[0].server.selectedIndex].text);')
+		js.append ('}')
+
 		js = js + [
 			'function doReset () {',
-			'  do_server (document.forms[0].origserver.value)',
+			'  do_dbms (document.forms[0].origdbms.value);',
+			'  do_server (document.forms[0].origdbms.value, document.forms[0].origserver.value)',
+			'  set (databases, document.forms[0].origdatabase.value)',
+			'}',
+			'function browse() {',
+			'var r = "rdbms=" + document.forms[0].DBMS1.options[document.forms[0].DBMS1.selectedIndex].text;',
+			'var d = "&database=" + document.forms[0].database.options[document.forms[0].database.selectedIndex].text;',
+			'var s = "&server=" + document.forms[0].server.options[document.forms[0].server.selectedIndex].text;',
+			'window.open ("browse.cgi?" + r + d + s);',
 			'}',
 			]
 		js.append ('</SCRIPT>')
